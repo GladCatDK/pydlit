940b058044ed43184dc225034cc6d2aee0a8dcdc
Denis Kurkov <denis.kurkov@maibornwolff.de>
2020-09-11 12:26:58 +0200

    Test pipeline: debugging

--- test.sh
+++ test.sh
@@ -2,9 +2,22 @@ docker run -d --net=host pydlit
 sleep 5
 TEMP_JOB_ID=$(curl -XPOST -H "Content-Type: application/json" -d @example/example_request.json http://localhost:5001/api/distribute/image | jq -r ".id")
 sleep 15
-RESULT=$(curl -vf -XGET http://localhost:5001/api/distribute/image/$TEMP_JOB_ID | jq -r ".value")
 
-if [[ "$RESULT" != "FINISHED" ]]; then
-echo "Test failed. Result: $RESULT"
+
+for i in `seq 1 10`;
+do
+RESULT=$(curl -vf -XGET http://localhost:5001/api/distribute/image/$TEMP_JOB_ID | jq -r ".value")
+if [[ "$RESULT" == "FINISHED" ]]; then
+echo "Job is $RESULT"
+exit 0
+fi
+if [[ "$RESULT" == "FAILED" ]]; then
+echo "Test $RESULT"
 exit 1
 fi
+if [[ "$RESULT" == "QUEUED" || "$RESULT" == "RUNNING" ]]; then
+echo "Job still $RESULT"
+echo "Waiting."
+sleep 5
+fi
+done
